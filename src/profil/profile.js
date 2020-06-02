2fb960d6defbcee16a5b45c7d2da9828b8a1b7f2
amine371 <63712749+amine371@users.noreply.github.com>
2020-06-02 11:10:17 +0100

    Update profile.js

--- src/profil/profile.js
+++ src/profil/profile.js
@@ -8,20 +8,18 @@ import proptypes from "prop-types";
     return(
         <div style={{margin:40}}>
             <h1 style={ { textTransform :'uppercase'}}>{props.fullname}</h1>
-            < div style={{color:'red'{props.profession}}}>
+            < div style={{color:'red'}}>{props.profession}
                 <div style={{display:'flex'}}>
             <p style ={{color='red'}}>{props.bio}</p>
              {props.children}
      </div>
             <a href="#" onClick={handleName} type={{color:'red'}}> Click</a>
-     Profile.defaultProps = {
+    )
+    }
+  Profile.defaultProps = {
     fullName: "No Name", 
     bio: "No Biography", 
     profession: "No Profession",
     <img src = {amine.jpg} alt = "amine.jpg"/>
-
-    )
-    }
-
 }
 export default profil;
